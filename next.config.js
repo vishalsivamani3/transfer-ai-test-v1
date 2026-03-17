ad6af22a9b56171cf709efc52a39a6883f3c4991
Vishal Sivamani <sivamani@uchicago.edu>
2026-03-17 15:35:07 +0000

    fix: ignore eslint warnings during vercel build

--- next.config.js
+++ next.config.js
@@ -3,6 +3,12 @@ const nextConfig = {
     images: {
         domains: ['images.unsplash.com'],
     },
+    eslint: {
+        ignoreDuringBuilds: true,
+    },
+    typescript: {
+        ignoreBuildErrors: false,
+    },
 }
 
-module.exports = nextConfig 
+module.exports = nextConfig
